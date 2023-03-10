09a3a191429208e8bbe07c83931a2c617db56304
Fue Lee <fueclee.12@gmail.com>
2023-03-09 19:43:09 -0600

    added 'pg' linking and added some preliminary HTML

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-
+const router = require('./routes/toDo.router.js');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 app.use(bodyParser.json());
 
 // ROUTES
-
+app.use('/weekend-to-do-app', router);
 
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
