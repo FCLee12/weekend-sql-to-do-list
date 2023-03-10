95f71440873c39125985ce72b21f2ec59c78bff6
Fue Lee <fueclee.12@gmail.com>
2023-03-10 15:17:57 -0600

    server / router side POST is functional

--- server/routes/toDo.router.js
+++ server/routes/toDo.router.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
-// Add a query to select koalas from database
 router.get("/", (req, res) => {
     const queryText = 'SELECT * FROM "toDos"';
     console.log("Submitting Query to DB:", queryText);
@@ -18,5 +17,25 @@ router.get("/", (req, res) => {
         res.sendStatus(500);
       });
   });
+
+// POST
+router.post('/', (req, res) => {
+    const newTask= req.body
+    console.log('New Task', newTask);
+
+    const queryText = `INSERT INTO "toDos" (task)
+    VALUES ($1); 
+    `;
+
+    pool.query(queryText, [newTask.task])
+    .then((result) => {
+        res.sendStatus(201)
+    })
+    .catch((error) => {
+        console.log(`Error making query ${queryText}`, error);
+        res.sendStatus(500);
+    })
+   
+});
 
 module.exports = router;
