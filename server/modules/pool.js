c5d1ca05ed0e1c5a2a1a4f1484e476647ef6156b
Fue Lee <fueclee.12@gmail.com>
2023-03-13 16:52:07 -0500

    adjusted pool.js for deployment on heroku and created database on bit.io then linked it into web app

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,9 +1,24 @@
 const pg = require("pg");
+let pool;
 
-const pool = new pg.Pool ({
-    database: "weekend-to-do-app",
-    host: "localhost",
-    port: 5432
-})
+// if there is a heroku URL available, use that
+if (process.env.DATABASE_URL) {
+    pool = new pg.Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    });
+  } else { // if there isn't, use localhost
+      pool = new pg.Pool({
+        database: "weekend-to-do-app", // DB NAME not table!
+        host: "localhost",
+        port: 5432,
+        // number of milliseconds a client must sit idle in the pool and not be checked out
+        // before it is disconnected from the backend and discarded
+        // default is 10000 (10 seconds) - set to 0 to disable auto-disconnection of idle clients
+        idleTimeoutMillis: 30000,
+      });
+  } 
 
 module.exports = pool;
